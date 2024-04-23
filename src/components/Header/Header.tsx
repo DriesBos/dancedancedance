c78214609bba30d37e96fcd80f73d732fa25bc2f
DriesBos <info@driesbos.com>
2024-04-23 12:21:20 +0100

    header active class

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,27 @@
+'use client'; // Because fetching active path
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import './Header.sass';
 
 export default function Header() {
+  const path = usePathname();
+
   return (
     <div className="header">
       <ul>
         <li>
-          <Link href="/">Home</Link>
+          <Link href="/" className={path.startsWith('/') ? 'active' : ''}>
+            Home
+          </Link>
         </li>
         <li>
-          <Link href="/about">About</Link>
+          <Link
+            href="/about"
+            className={path.startsWith('/about') ? 'active' : ''}
+          >
+            About
+          </Link>
         </li>
       </ul>
     </div>
