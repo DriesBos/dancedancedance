eb9ec9fe425d38b14ce8caa1b6754b6c6414fc24
DriesBos <info@driesbos.com>
2024-05-23 09:42:03 +0100

    show home page

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+import { ISbStoriesParams, getStoryblokApi } from '@storyblok/react/rsc';
+import StoryblokStory from '@storyblok/react/story';
+
+export default async function Home() {
+  const { data } = await fetchData();
+
+  return (
+    <div>
+      <p>HOME</p>
+      <StoryblokStory story={data.story} />
+    </div>
+  );
+}
+
+export async function fetchData() {
+  let sbParams: ISbStoriesParams = { version: 'draft' };
+
+  const storyblokApi = getStoryblokApi();
+  return await storyblokApi.get(`cdn/stories/home`, sbParams, {
+    cache: 'no-store',
+  });
+}
