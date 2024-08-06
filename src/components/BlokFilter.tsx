79d519c51b9abe01327201432c43a8f2604bd666
DriesBos <info@driesbos.com>
2024-08-06 16:26:58 +0100

    style: responsiveness

--- src/components/BlokFilter.tsx
+++ src/components/BlokFilter.tsx
@@ -13,24 +13,24 @@ export default function BlokFilter() {
         <div className="blok blok-Filter">
           <BlokTopPanel />
           <Row>
-            <div className="column" data-inactive={true}>
+            <div className="column column-Year" data-inactive={true}>
               Date
             </div>
-            <div className="column" data-inactive={true}>
+            <div className="column column-Title" data-inactive={true}>
               Project
             </div>
-            <div className="column" data-inactive={true}>
+            <div className="column column-Client" data-inactive={true}>
               Role
             </div>
-            <div className="column" data-inactive={true}>
+            <div className="column column-Category" data-inactive={true}>
               Client type
             </div>
-            <div className="column" data-inactive={true}>
+            {/* <div className="column" data-inactive={true}>
               Project Type
             </div>
             <div className="column" data-inactive={true}>
               Tech
-            </div>
+            </div> */}
             <div className="column column-Icons" data-inactive={true}>
               <div className="icon">
                 <IconAbout />
