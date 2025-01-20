6efb5c51301419e858d37312ce444b3ce0492f54
DriesBos <info@driesbos.com>
2025-01-20 09:57:33 +0000

    clean

--- src/components/ThemeBackground.tsx
+++ src/components/ThemeBackground.tsx
@@ -4,14 +4,7 @@ import backgroundImage from '../assets/images/rosanne.jpg';
 export default function PaThemeBackgroundge() {
   return (
     <div className="themeBackground">
-      <Image
-        src={backgroundImage}
-        alt=""
-        // width={500} automatically provided
-        // height={500} automatically provided
-        // blurDataURL="data:..." automatically provided
-        // placeholder="blur" // Optional blur-up while loading
-      />
+      <Image src={backgroundImage} alt="" />
     </div>
   );
 }
