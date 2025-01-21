2d3d40d6368edeb50585982ac8479a264d2423c2
DriesBos <info@driesbos.com>
2025-01-21 18:08:43 +0000

    gsap optimisation

--- src/helpers/runGSAP.js
+++ src/helpers/runGSAP.js
@@ -6,17 +6,20 @@ import { useGSAP } from '@gsap/react';
 gsap.registerPlugin(useGSAP);
 
 function RunGsap() {
-  useGSAP(() => {
-    gsap.to('.blok', {
-      opacity: 1,
-      y: 0,
-      ease: 'power1.inOut',
-      duration: 0.33,
-      stagger: {
-        amount: 0.66,
-      },
-    });
-  });
+  useGSAP(
+    () => {
+      gsap.to('.blok', {
+        opacity: 1,
+        y: 0,
+        ease: 'power1.inOut',
+        duration: 0.33,
+        stagger: {
+          amount: 0.66,
+        },
+      });
+    },
+    { revertOnUpdate: true }
+  );
 
   return null;
 }
--- src/helpers/runLaser.js
+++ src/helpers/runLaser.js
@@ -6,24 +6,27 @@ import { useGSAP } from '@gsap/react';
 gsap.registerPlugin(useGSAP);
 
 function RunLaser() {
-  useGSAP(() => {
-    gsap.set('.laserBlok-Line', {
-      width: 0,
-      height: 0,
-      opacity: 0,
-    });
-    gsap.to('.laserBlok-Line', {
-      opacity: 1,
-      duration: 0,
-      ease: 'none',
-    });
-    gsap.to('.laserBlok-Line', {
-      width: '105vw',
-      height: '105vh',
-      duration: 0.66,
-      ease: 'expo.out',
-    });
-  });
+  useGSAP(
+    () => {
+      gsap.set('.laserBlok-Line', {
+        width: 0,
+        height: 0,
+        opacity: 0,
+      });
+      gsap.to('.laserBlok-Line', {
+        opacity: 1,
+        duration: 0,
+        ease: 'none',
+      });
+      gsap.to('.laserBlok-Line', {
+        width: '105vw',
+        height: '105vh',
+        duration: 0.66,
+        ease: 'expo.out',
+      });
+    },
+    { revertOnUpdate: true }
+  );
 
   return null;
 }
