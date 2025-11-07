ca3f7428ebe94f5b7ceef5967d755f853651cba5
DriesBos <info@driesbos.com>
2025-11-07 15:58:09 +0900

    Added page thumbnail

--- src/components/storyblok/Page.tsx
+++ src/components/storyblok/Page.tsx
@@ -14,7 +14,7 @@ interface PageProps {
 
 const Page: React.FunctionComponent<PageProps> = ({ blok }) => {
   return (
-    <div className="page" {...storyblokEditable(blok)}>
+    <div className="page page-General" {...storyblokEditable(blok)}>
       {blok.body.map((nestedBlok: any) => (
         <StoryblokServerComponent blok={nestedBlok} key={nestedBlok._uid} />
       ))}
--- src/components/storyblok/PageProject.tsx
+++ src/components/storyblok/PageProject.tsx
@@ -5,9 +5,15 @@ import {
   storyblokEditable,
   StoryblokServerComponent,
 } from '@storyblok/react/rsc';
+import Image from 'next/image';
 
 interface SbPageProjectData extends SbBlokData {
   body: SbBlokData[];
+  thumbnail?: {
+    filename: string;
+    alt?: string;
+  };
+  title?: string;
 }
 
 interface ProjectProps {
@@ -15,8 +21,22 @@ interface ProjectProps {
 }
 
 const PageProject = ({ blok }: ProjectProps) => {
+  console.log('PageProject', blok.thumbnail);
+
   return (
     <div className="page page-Project" {...storyblokEditable(blok)}>
+      {}
+      <div className="blok page-Project-Thumbnail">
+        {blok.thumbnail && (
+          <Image
+            src={blok.thumbnail.filename}
+            alt={blok.title || 'Project Thumbnail'}
+            width={600}
+            height={400}
+            style={{ width: '100%', height: 'auto' }}
+          />
+        )}
+      </div>
       {blok.body.map((nestedBlok: any) => (
         <StoryblokServerComponent blok={nestedBlok} key={nestedBlok._uid} />
       ))}
