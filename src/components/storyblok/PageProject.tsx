bf29d36fe882aef6cf79c4f3fdaea0378835b414
DriesBos <info@driesbos.com>
2025-11-10 11:35:56 +0900

    cleanup

--- src/components/storyblok/PageProject.tsx
+++ src/components/storyblok/PageProject.tsx
@@ -9,10 +9,6 @@ import Image from 'next/image';
 
 interface SbPageProjectData extends SbBlokData {
   body: SbBlokData[];
-  thumbnail?: {
-    filename: string;
-    alt?: string;
-  };
   title?: string;
 }
 
@@ -26,17 +22,6 @@ const PageProject = ({ blok }: ProjectProps) => {
   return (
     <div className="page page-Project" {...storyblokEditable(blok)}>
       {}
-      <div className="blok page-Project-Thumbnail">
-        {blok.thumbnail && (
-          <Image
-            src={blok.thumbnail.filename}
-            alt={blok.title || 'Project Thumbnail'}
-            width={600}
-            height={400}
-            style={{ width: '100%', height: 'auto' }}
-          />
-        )}
-      </div>
       {blok.body.map((nestedBlok: any) => (
         <StoryblokServerComponent blok={nestedBlok} key={nestedBlok._uid} />
       ))}
