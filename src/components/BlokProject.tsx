c94cb80413e58085c3de0d92162d073524500107
DriesBos <info@driesbos.com>
2025-11-11 21:49:22 +0900

    Move comp to server rendering

--- src/components/BlokProject.tsx
+++ src/components/BlokProject.tsx
@@ -1,10 +1,6 @@
-'use client';
-
 import Link from 'next/link';
 import IconArrow from '@/components/Icons/IconArrow';
 import Row from './Row';
-import { useStore } from '@/store/store';
-import Image from 'next/image';
 
 interface Props {
   slug?: String;
@@ -30,40 +26,23 @@ const BlokProject = ({
   active,
   images,
 }: Props) => {
-  const index = useStore((state: any) => state.index);
-
   return (
     <Link
       className={`blok blok-Project blok-Animate ${active ? '' : 'inActive'}`}
       href={`/projects/${slug}`}
     >
-      {index === 'TXT' ? (
-        <Row>
-          <div className="column column-Year">{year}</div>
-          <div className="column column-Project">{title}</div>
-          <div className="column column-Category">{category}</div>
-          <div className="column column-Role">{role}</div>
-          <div className="column column-Location">{location}</div>
-          <div className="column column-Icons">
-            <div className="icon">
-              <IconArrow />
-            </div>
+      <Row>
+        <div className="column column-Year">{year}</div>
+        <div className="column column-Project">{title}</div>
+        <div className="column column-Category">{category}</div>
+        <div className="column column-Role">{role}</div>
+        <div className="column column-Location">{location}</div>
+        <div className="column column-Icons">
+          <div className="icon">
+            <IconArrow />
           </div>
-        </Row>
-      ) : (
-        <Row>
-          <div className="column column-Thumbnail">
-            <Image
-              src={images[0].filename}
-              alt={images[0].alt}
-              width={0}
-              height={0}
-              sizes="100vw"
-              style={{ width: '100%', height: 'auto' }}
-            />
-          </div>
-        </Row>
-      )}
+        </div>
+      </Row>
     </Link>
   );
 };
