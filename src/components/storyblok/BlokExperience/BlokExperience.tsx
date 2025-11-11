a5f4fb16a66d1860b3e1407b8be3a33a5a6d75ef
DriesBos <info@driesbos.com>
2025-11-11 21:02:00 +0900

    Added exp section to about page

--- src/components/storyblok/BlokExperience/BlokExperience.tsx
+++ src/components/storyblok/BlokExperience/BlokExperience.tsx
@@ -1,6 +1,7 @@
 import styles from './BlokExperience.module.sass';
 import { SbBlokData } from '@storyblok/react/rsc';
 import BlokSidePanels from '../../BlokSides';
+import Row from '@/components/Row';
 
 interface SbPageData extends SbBlokData {
   body: SbBlokData[];
@@ -12,61 +13,56 @@ interface BlokProps {
 
 const BlokExperience = ({ blok }: BlokProps) => {
   return (
-    <div className="blok-Exp blok-Animate">
-      <BlokSidePanels />
-      <div>
-        <div className={styles.BlokName}>Experience</div>
-        <div className={styles.Content}>
-          <div className={styles.Row}>
-            <div className={styles.RowItem}>
-              <span className={styles.RowPeriod}>2025 — Present</span>
-            </div>
-            <div className={styles.RowItem}>
-              <span className={styles.RowName}>Dries Bos Studio</span>
-              <span className={styles.RowRole}>Freelance</span>
-            </div>
+    <div className={`blok blok-Exp blok-Animate`}>
+      <div className={styles.blokName}>Experience</div>
+      <div className={styles.content}>
+        <div className={styles.row}>
+          <div className={styles.rowItem}>
+            <span className={styles.rowPeriod}>2025 — Present</span>
           </div>
-
-          <div className={styles.Row}>
-            <div className={styles.RowItem}>
-              <span className={styles.RowPeriod}>2022 — 2025</span>
-            </div>
-            <div className={styles.RowItem}>
-              <span className={styles.RowName}>Mmerch</span>
-              <span className={styles.RowRole}>Senior Frontend Developer</span>
-            </div>
+          <div className={styles.rowItem}>
+            <span className={styles.rowName}>Dries Bos Studio</span>
+            <span className={styles.rowRole}>Freelance</span>
           </div>
+        </div>
 
-          <div className={styles.Row}>
-            <div className={styles.RowItem}>
-              <span className={styles.RowPeriod}>2020 — 2022</span>
-            </div>
-            <div className={styles.RowItem}>
-              <span className={styles.RowName}>Anatha</span>
-              <span className={styles.RowRole}>Lead UX Design</span>
-            </div>
+        <div className={styles.row}>
+          <div className={styles.rowItem}>
+            <span className={styles.rowPeriod}>2022 — 2025</span>
           </div>
-
-          <div className={styles.Row}>
-            <div className={styles.RowItem}>
-              <span className={styles.RowPeriod}>2017 — 2019</span>
-            </div>
-            <div className={styles.RowItem}>
-              <span className={styles.RowName}>Fotomat</span>
-              <span className={styles.RowRole}>Frontend Developer</span>
-            </div>
+          <div className={styles.rowItem}>
+            <span className={styles.rowName}>Mmerch</span>
+            <span className={styles.rowRole}>Senior Frontend Developer</span>
           </div>
+        </div>
 
-          <div className={styles.Row}>
-            <div className={styles.RowItem}>
-              <span className={styles.RowPeriod}>2011 — 2015</span>
-            </div>
-            <div className={styles.RowItem}>
-              <span className={styles.RowName}>Close My Eyes</span>
-              <span className={styles.RowRole}>
-                Frontend Developer + Founder
-              </span>
-            </div>
+        <div className={styles.row}>
+          <div className={styles.rowItem}>
+            <span className={styles.rowPeriod}>2020 — 2022</span>
+          </div>
+          <div className={styles.rowItem}>
+            <span className={styles.rowName}>Anatha</span>
+            <span className={styles.rowRole}>Lead UX Design</span>
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div className={styles.rowItem}>
+            <span className={styles.rowPeriod}>2017 — 2019</span>
+          </div>
+          <div className={styles.rowItem}>
+            <span className={styles.rowName}>Fotomat</span>
+            <span className={styles.rowRole}>Frontend Developer</span>
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div className={styles.rowItem}>
+            <span className={styles.rowPeriod}>2011 — 2015</span>
+          </div>
+          <div className={styles.rowItem}>
+            <span className={styles.rowName}>Close My Eyes</span>
+            <span className={styles.rowRole}>Frontend Developer + Founder</span>
           </div>
         </div>
       </div>
