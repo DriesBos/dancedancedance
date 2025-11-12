99b5bc02a95eea0b1d07ba24b0422ee8401802c1
DriesBos <info@driesbos.com>
2025-11-12 14:42:14 +0900

    fix: markdown link styling

--- src/components/TheMarkdown/TheMarkdown.tsx
+++ src/components/TheMarkdown/TheMarkdown.tsx
@@ -1,6 +1,11 @@
+'use client';
+
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import rehypeRaw from 'rehype-raw';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import styles from './TheMarkdown.module.sass';
 
 interface MarkdownProps {
   content: string;
@@ -11,12 +16,21 @@ const Markdown: React.FunctionComponent<MarkdownProps> = ({
   content,
   className = '',
 }) => {
+  useGSAP(() => {
+    gsap.to('.markdown', {
+      '--var': '100%',
+      duration: 0.66,
+      delay: 0.33,
+      ease: 'ease',
+    });
+  });
+
   if (!content) {
     return null;
   }
 
   return (
-    <div className={`${className}`}>
+    <div className={`${className} ${styles.markdown} markdown`}>
       <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
         {content}
       </ReactMarkdown>
