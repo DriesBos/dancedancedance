f3c0fc8cebfba9ff50f373ed8e1fde4313f51a9f
DriesBos <info@driesbos.com>
2025-11-25 18:04:01 +0900

    added new icon comp

---
+++ src/components/Icons/IconLinkOutside.tsx
@@ -0,0 +1,12 @@
+const IconLinkOutside = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="none">
+      <path
+        fill="currentColor"
+        d="M12 2v2H4v17h17v-8h2v10H2V2h10Zm11 8h-2V5.414L9.707 16.707l-1.414-1.414L19.586 4H15V2h8v8Z"
+      />
+    </svg>
+  );
+};
+
+export default IconLinkOutside;
