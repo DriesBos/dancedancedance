145785bd983323332870b3bcc090f0e44d488dbf
DriesBos <info@driesbos.com>
2025-11-25 18:04:36 +0900

    update link icon to viewBox

--- src/components/Icons/IconLinkOutside.tsx
+++ src/components/Icons/IconLinkOutside.tsx
@@ -1,6 +1,6 @@
 const IconLinkOutside = () => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="none">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25" fill="none">
       <path
         fill="currentColor"
         d="M12 2v2H4v17h17v-8h2v10H2V2h10Zm11 8h-2V5.414L9.707 16.707l-1.414-1.414L19.586 4H15V2h8v8Z"
