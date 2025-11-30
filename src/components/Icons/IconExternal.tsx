07b213b2044ff31a410143b2419a6a3c1e51ffb1
DriesBos <info@driesbos.com>
2025-11-30 15:06:34 +0900

    update icon

--- src/components/Icons/IconExternal.tsx
+++ src/components/Icons/IconExternal.tsx
@@ -3,7 +3,7 @@ const IconExternal = () => {
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 12 12">
       <path
         fill="currentColor"
-        d="M11 8H9V4.408L5.454 7.954 4.04 6.54 7.58 3H4V1h7v7Z"
+        d="M11 8H9.5V3.56L5.071 7.99l-1.06-1.061L8.44 2.5H4V1h7v7Z"
       />
     </svg>
   );
