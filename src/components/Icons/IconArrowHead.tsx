190392c731323862a72229b16bc08a5dbd31873c
DriesBos <info@driesbos.com>
2025-11-30 17:08:05 +0900

    fix: svg syntax warnings

--- src/components/Icons/IconArrowHead.tsx
+++ src/components/Icons/IconArrowHead.tsx
@@ -7,9 +7,9 @@ const IconArrowHead = ({ className }: Props) => {
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 25 50">
       <path
         fill="currentColor"
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M11.503 3.822 1.91 13.424.501 12.01 11.088 1.409l-.002-.002L12.494 0l.002.002V0l1.412 1.413v.002L24.489 12.01l-1.406 1.407L13.5 3.822h-1.996Z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       />
     </svg>
   );
